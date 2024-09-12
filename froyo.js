5ae1e76aff61dfc8a4fa7771f230abfbf1051c22
Matthew De Andrade <fayrfax@gmail.com>
2024-09-11 20:46:49 -0400

    Fixed bug! Changed counter to an array of numbers! It was previously an array of strings and my code wasn't working right!

--- froyo.js
+++ froyo.js
@@ -1,12 +1,24 @@
 const froyo={
-counter:"0,0,0"
+counter:[0,0,0],
 //counts the number of times each flavor has been called in alphabetical order of flavors.
-//Example: "2, 1, 0" means that 2 coffee froyos, 1 strawberry froyo, and no vanilla froyos were ordered."
+//Example: "2, 1, 0" means that 2 coffee froyos, 1 strawberry froyo, and no vanilla froyos were ordered.
+checkout:"",//this string holds the message for the user's order. The function search will take in the order and parse for flavors.
 };
 
-function readOrder(order){let message="You have ordered " + order.counter[0] + " coffee, " + order.counter[2] + " strawberry " + order.counter[2] + " vanilla froyos";
+function readOrder(order){let message="You have ordered " + order.counter[0] + " coffee, " + order.counter[2] + " strawberry " + order.counter[2] + " vanilla froyos.";
     return message;   
 }
 
 alert(readOrder(froyo));
-function search(order){}
+froyo.checkout="coffee, coffee, coffee";
+
+
+function search(order){
+    /*const bill=order.checkout; for (let i=0; i<bill.length; i++) if (bill.checkout[i]==="coffee") */ order.counter[0]++; 
+}
+
+//search(froyo);
+
+/*alert(froyo.checkout);*/
+froyo.counter[0]++;
+alert(froyo.counter[0]);
